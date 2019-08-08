69b57fb95c3ecfaa94c54e954b3112bda80d7a6c
Abhishek Minz <51510507+abhishekminz@users.noreply.github.com>
2019-08-08 12:43:45 +0530

    Updates

---
+++ precache-manifest.ec23f449fcfe6c054c04c268e75abb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e959b0d5f3e9790defbb2d0c7207b58",
+    "url": "/facedetector-frontend/index.html"
+  },
+  {
+    "revision": "7812395f23a7ebcb6e34",
+    "url": "/facedetector-frontend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "07d4e5f72ad6d61ad78e",
+    "url": "/facedetector-frontend/static/css/main.8fb7431b.chunk.css"
+  },
+  {
+    "revision": "7812395f23a7ebcb6e34",
+    "url": "/facedetector-frontend/static/js/2.31375f3c.chunk.js"
+  },
+  {
+    "revision": "07d4e5f72ad6d61ad78e",
+    "url": "/facedetector-frontend/static/js/main.6945097d.chunk.js"
+  },
+  {
+    "revision": "ea1c2d168cca234bbc31",
+    "url": "/facedetector-frontend/static/js/runtime~main.1ec1507e.js"
+  }
+]);
